3156c68b35adf0779498028d2142208b3da28dd6
PawkaHub <pieterjparker@gmail.com>
2017-02-19 00:55:29 -0700

    Added form status messages

--- js/guestForm.js
+++ js/guestForm.js
@@ -5,11 +5,70 @@ var isAttending = function(value) {
 
 $(document).ready(function() {
   // Handle Guest RSVP Form Submission
-  var guestForm = $('#guest-rsvp-form');
   var db = firebase.database();
+
+  // Elements
+  var guestForm = $('#guest-rsvp-form');
+  var loadingMessage = $('.message.loading');
+  var errorMessage = $('.message.error');
+  var successMessage = $('.message.success');
+
+  var messageDelay = 2000;
+
+  // Message handlers
+  var clearMessages = function() {
+    errorMessage.removeClass('active');
+    successMessage.removeClass('active');
+    loadingMessage.removeClass('active');
+  }
+
+  var showLoading = function(message) {
+    if (!message) {
+      message = 'Confirming Your RSVP...';
+    }
+    loadingMessage.text(message);
+
+    errorMessage.removeClass('active');
+    successMessage.removeClass('active');
+    loadingMessage.addClass('active');
+  }
+
+  var showError = function(message, disableDelay) {
+    if (!message) {
+      message = 'There was an error confirming your RSVP.';
+    }
+    errorMessage.text(message);
+
+    var delay = messageDelay;
+    if (disableDelay) { delay = 0; }
+
+    setTimeout(function() {
+      successMessage.removeClass('active');
+      loadingMessage.removeClass('active');
+      errorMessage.addClass('active');
+    }, delay);
+  }
+
+  var showSuccess = function(message, disableDelay) {
+    if (!message) {
+      message = 'RSVP Confirmed!';
+    }
+    successMessage.text(message);
+
+    var delay = messageDelay;
+    if (disableDelay) { delay = 0; }
+
+    setTimeout(function() {
+      loadingMessage.removeClass('active');
+      errorMessage.removeClass('active');
+      successMessage.addClass('active');
+    }, delay);
+  }
 
   guestForm.submit(function(e) {
     e.preventDefault();
+
+    // Serialize form data
     var data = $(this).serializeObject();
     var firstName = data['first-name'];
     var lastName = data['last-name'];
@@ -23,8 +82,15 @@ $(document).ready(function() {
     console.log('attendingReception', attendingReception);
     console.log('message', message);
 
+    // Get the fullName for this user
     var fullName = firstName.toLowerCase() + '-' + lastName.toLowerCase();
     console.log('fullName', fullName);
+
+    if (!firstName) return showError('Please provide a first name.', true);
+    if (!lastName) return showError('Please provide a last name.', true);
+
+    // Show a loading message
+    showLoading();
 
     // Check for if the fullName exists in our invited collection
     db.ref('invited/' + fullName).once('value').then(function(snap) {
@@ -41,14 +107,31 @@ $(document).ready(function() {
         };
 
         // Persist the RSVP to the server
-        db.ref('rsvp/' + fullName).set(payload).then(function(result) {
-          console.log('RSVP successful', result);
-        }).catch(function(error) {
-          console.log('RSVP error', error);
+        // Check for if ref already exists before confirming it.
+        db.ref('rsvp/' + fullName).once('value').then(function(snap) {
+          if (snap.exists()) {
+            console.log('RSVP already confirmed, show success message');
+            showSuccess();
+          } else {
+            console.log('RSVP not created yet, create it.');
+            db.ref('rsvp/' + fullName).set(payload).then(function(result) {
+              console.log('RSVP successful', result);
+              showSuccess();
+            }).catch(function(error) {
+              console.log('RSVP error', error);
+              showError();
+            });
+          }
         });
       } else {
         console.log('User not invited. Throw error.');
+        showError();
       }
     });
+  });
+
+  guestForm.on('keyup', function(e) {
+    // Clear any messages that are currently being displayed
+    clearMessages();
   });
 });
